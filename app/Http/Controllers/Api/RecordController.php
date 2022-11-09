ca6f7ea33bc87d4df2ae1a3d756dbb7f409c6041
Acha <serv661@gmail.com>
2022-11-10 01:26:53 +0800

    [更新] 增加Post => Record , api

---
+++ app/Http/Controllers/Api/RecordController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreRecordRequest;
+use App\Models\Record;
+use Illuminate\Http\Request;
+
+class RecordController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $record = Record::all();
+
+        return response()->json([
+            'status' => true,
+            'posts' => $record
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreRecordRequest $request)
+    {
+        $record = Record::create($request->all());
+
+        return response()->json([
+            'status' => true,
+            'message' => "Post Created successfully!",
+            'post' => $record
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Record  $record
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Record $record)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Record  $record
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Record $record)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Record  $record
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Record $record)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Record  $record
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Record $record)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_11_09_170716_create_records_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->id('rdId')->comment('履歷ID');
+            $table->string('rdName',50)->comment('志工姓名');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('records');
+    }
+};
